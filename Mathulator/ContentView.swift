39a8c5409e8d0839ef75b9edde8bb48e805edef3
Joachim Seland Graff <Joachim.Seland.Graff@gmail.com>
2025-08-31 20:01:46 +0200

    Refactored HighScoresView to separate file

--- Mathulator/ContentView.swift
+++ Mathulator/ContentView.swift
@@ -267,80 +267,11 @@ var body: some View {
     
     // MARK: - HighScoreView
     
-    struct HighScoresView: View {
-        @ObservedObject var highScores: HighScores
 
-        var body: some View {
-            GeometryReader { geometry in
-                NavigationView {
-                    VStack {
-                        
-                        HStack {
-                            // Fixed top row in High scores
-                            Text("")
-                                .frame(width: geometry.size.width * 0.15, alignment: .center)
-                                .font(.caption)
-                                .padding(4)
-                                .clipShape(RoundedRectangle(cornerRadius: 5))
-                            // Show each operator on top of their columns
-                            ForEach(Operator.allCases, id: \.self) { op in
-                                Text(op.rawValue)
-                                    .frame(width: geometry.size.width * 0.16, height: geometry.size.height * 0.06, alignment: .center)
-                                    .font(.title2)
-//                                    .padding(1)
-                                    .foregroundColor(Color.white)
-                                    .background(
-                                            ZStack {
-                                                Color.blue
-                                                Color.black.opacity(0.3)
-                                            }
-                                        )//.background(Color.blue.opacity(0.8))
-                                    .clipShape(RoundedRectangle(cornerRadius: 5))
-                            }
-                        }
-                        
-                        // List entire high score table
-                        ScrollView {
-                            VStack(alignment: .leading, spacing: 12) {
-                                
-                                ForEach(HighScores.difficulties, id: \.self) { difficulty in
-                                    HStack(spacing: 4) {
-                                        // Use custom colors for each row heading
-                                        Text("\(difficulty)")
-                                            .frame(width: geometry.size.width * 0.15, alignment: .center)
-                                            .font(.caption)
-                                            .padding(4)
-                                            .foregroundColor(Color.black)
-                                            .background(ContentView.difficultyColor(for: Double(difficulty)))
-                                            .clipShape(RoundedRectangle(cornerRadius: 5))
-                                        
-                                        ForEach(Operator.allCases, id: \.self) { op in
-                                            let score = highScores.highScore(for: difficulty, op: op)
-                                            let symbols = trophyText(for: score)
-                                            Text("\(symbols)")
-                                                .frame(width: geometry.size.width * 0.15)  // 4 operators × 0.18 = ~0.72 + 0.15 = ~0.87
-                                                .font(.caption)
-                                                .padding(4)
-                                                // Custom colors for various score levels
-                                                .background(highScoreBackgroundColor(for: score))
-                                                .clipShape(RoundedRectangle(cornerRadius: 5))
-                                        }
-                                    }
-                                }
-                            }
-                            .padding()
-                        }
-                    }
-                    .navigationTitle("\u{1F3C6}\u{1F3C6}\u{1F3C6}")  // "\u{1F3C6} High scores \u{1F3C6}"Unicode "Trophy"
-                    .navigationBarTitleDisplayMode(.inline)
-                }
-            }
-        }
-    }
     
     var scoreSymbols: String {
         let score = highScores.highScore(for: Int(difficulty), op: selectedOperator)
-        return ContentView.trophyText(for: score)
+        return HighScoresView.trophyText(for: score)
     }
     // MARK: - Helpers
 
@@ -385,16 +316,7 @@ var body: some View {
         return Int((Double(correct) / Double(history.count)) * 100)
     }
     
-    static func highScoreBackgroundColor(for score: Int) -> Color {
-        switch score {
-        case 0:
-            return Color.gray.opacity(0.1)
-        case 1...:
-            return Color.blue.opacity(0.3)
-        default:
-            return Color.gray.opacity(0.1)
-        }
-    }
+
 
     static func difficultySaturation(for difficulty: Double) -> Double {
         return Double((difficulty - minDifficulty) / (maxDifficulty - minDifficulty))
@@ -404,18 +326,4 @@ var body: some View {
         return Color(hue: 0.1, saturation: saturation, brightness: 0.94)
     }
     
-    static func trophyText(for score: Int) -> String {
-        switch score {
-        case 0...4:
-            return ""
-        case 5...11:
-            return "\u{1F31F}"  // Unicode star
-        case 12...19:
-            return "\u{1F31F} \u{1F3C5}"  // Unicode star, medal
-        case 20...:
-            return "\u{1F31F}\u{1F3C5}\u{1F451}"  // Unicode star, medal, crown
-        default:
-            return "\(score)"
-        }
-    }
 }
