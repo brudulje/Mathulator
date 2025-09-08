ee6b078c1e06d21311a3148d2d80763acb3ca6d0
Joachim Seland Graff <Joachim.Seland.Graff@gmail.com>
2025-09-08 21:44:39 +0200

    Improved detailed highscore modal

--- Mathulator/HighScoreDetailView.swift
+++ Mathulator/HighScoreDetailView.swift
@@ -14,51 +14,61 @@ struct HighScoreDetailView: View {
     var body: some View {
         GeometryReader { geometry in
             VStack(spacing: 20) {
-                Text("\u{1F3C6}")
-                    .font(.title)
-                
-//                Text("Difficulty: \(difficulty)")
-                Text("\(op.rawValue)")
-                    .frame(width: geometry.size.width * 0.21, height: geometry.size.width * 0.13)
-                    .font(.title)
-                    .background(ZStack{
-                        Color.blue
-                        Color.black.opacity(0.4)
-                    })//.opacity(0.4) )
-                    .foregroundColor(Color.white)
-                    .clipShape(RoundedRectangle(cornerRadius: 5))
-                ZStack {
+                HStack{
+                    Spacer()
+                    Text("\u{1F3C6}")
+                        .font(.title)
+                    Spacer()
+                }
+                HStack{
+                    Text("Operator:").font(.title2)
+                    Text("\(op.rawValue)")
+                        .frame(width: geometry.size.width * 0.21, height: geometry.size.width * 0.13, alignment: .center)
+                        .font(.title)
+                        .background(ZStack{
+                            Color.blue
+                            Color.black.opacity(0.4)
+                        })//.opacity(0.4) )
+                        .foregroundColor(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                }
+                HStack {
+                    
+                    Text("Difficulty:").font(.title2)
+                    Text("\(difficulty)")
+                        .frame(width: geometry.size.width * 0.21, height: geometry.size.width * 0.13, alignment: .center)
+                        .font(.title2)
+//                        .padding(4)
+                        .foregroundColor(Color.black)
+                        .background(ContentView.difficultyColor(for: Double(difficulty)))
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                }
+                HStack{
                     let score = highScores.highScore(for: difficulty, op: op)
                     let symbols = HighScoresView.trophyText(for: score)
-
-                    HStack{
-                        Text("\(difficulty)")
-                            .frame(width: geometry.size.width * 0.21, alignment: .center)
-                            .font(.caption)
-                            .padding(4)
-                            .foregroundColor(Color.black)
-                            .background(ContentView.difficultyColor(for: Double(difficulty)))
-                            .clipShape(RoundedRectangle(cornerRadius: 5))
-                        Spacer()
-                        Text("\(score)").font(.title)
-                    }
+                    
+                    Text("Streak:").font(.title2)
+                    Text("\(score)").font(.title2)
+//                    let score = highScores.highScore(for: difficulty, op: op)
+                    
+                    Text("Trophies:").font(.title2)
                     Text(symbols)
-                        .frame(width: geometry.size.width * 0.21, height: geometry.size.width * 0.13)
-                        .font(.caption)
+                        .frame(width: geometry.size.width * 0.26, height: geometry.size.width * 0.13)
+                        .font(.title3)
                         .padding(4)
                         .background(HighScoresView.highScoreBackgroundColor(for: score))
                         .clipShape(RoundedRectangle(cornerRadius: 5))
                 }
 //                Text("Operator: \(op.rawValue)")
                 
-                let score = highScores.highScore(for: difficulty, op: op)
+//                let score = highScores.highScore(for: difficulty, op: op)
 //                Text("Current Streak: \(score)")
 //                    .font(.largeTitle)
                 
                 Button(role: .destructive) {
                     highScores.reset(difficulty: difficulty, op: op)
                 } label: {
-                    Text("\(score) \u{2192} 0")
+                    Text("Reset Streak")
                         .padding()
                         .frame(maxWidth: geometry.size.width * 0.55)
                         .background(Color.red.opacity(0.8))
